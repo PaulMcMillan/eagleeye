9a7f02394c83ae2b563687d6ee45768bda986d74
Paul McMillan <paul.mcmillan@nebula.com>
2013-06-11 01:18:10 -0400

    License and packaging work

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(
+    name="eagleeye",
+    version=version,
+    description="Distributed tool for screenshotting webpages.",
+    long_description=open("README.rst").read(),
+    url='https://github.com/PaulMcMillan/eagleeye',
+    license="Simplified BSD",
+    author="Paul McMillan",
+    author_email="paul@mcmillan.ws",
+
+    install_requires=[
+        "tasa",
+        "selenium",
+        "pyvirtualdisplay",
+        ],
+    extras_require={
+        "tests": [
+            "pep8",
+            "pylint",
+            "pytest",
+            "pytest-cov",
+            ],
+        },
+    tests_require=[
+        "pep8",
+        "pytest",
+        "pytest-cov",
+        ],
+
+    packages=find_packages(),
+
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ])
+
+
+
